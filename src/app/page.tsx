da523cc9efe8fc92ed0a02352feef44a5ce49600
Sourav59580 <sourav.1121@zenmonk.tech>
2024-08-06 12:28:54 +0530

    page issue fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,21 @@ import * as React from "react";
 import { Box, Button, Typography } from "@mui/material";
 
 import style from "./page.module.css";
-
-const Page = ({
-  decorative = "All-in-One",
-  title = "The Developer's Toolkit",
-  subtitle = "Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
-}: {
-  decorative?: React.ReactNode;
-  title?: React.ReactNode;
-  subtitle?: React.ReactNode;
-}) => {
+const Page: React.FC = () => {
+  const obj = {
+    decorative: "All-in-One",
+    title: "The Developer's Toolkit",
+    subtitle: "Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
+  }
+  const { decorative, title, subtitle } = obj;
   return (
     <Box className={`${style.herosection} flex flex-col justify-center items-center text-center`} gap={2} my={6} height="50vh">
-      <Typography variant="paragraphLg" color="primary" className="text-uppercase" fontWeight="fontWeightMedium">{decorative}</Typography>
-      <Typography variant="h1" fontSize="3rem !important" fontWeight="fontWeightBold">{title}</Typography>
-      <Typography variant="h6" fontWeight="fontWeightLight" maxWidth="54ch" mb={1}>{subtitle}</Typography>
-
-      <Button component="a" href="/docs" variant="contained" color="primary" sx={{px: 3, py: 1.5}}>Get Started</Button>
+      <Typography variant="paragraphLg" color="primary" className="text-uppercase" fontWeight="fontWeightMedium">{decorative || ""}</Typography>
+      <Typography variant="h1" fontSize="3rem !important" fontWeight="fontWeightBold">{title || ""}</Typography>
+      <Typography variant="h6" fontWeight="fontWeightLight" maxWidth="54ch" mb={1}>{subtitle || ""}</Typography>
+      <Button component="a" href="/docs" variant="contained" color="primary" sx={{ px: 3, py: 1.5 }}>Get Started</Button>
     </Box>
   );
-}
+};
 
 export default Page;
